9c74bdb93feeb32642c42988dac51d1d2615b944
doyun <do0715@naver.com>
2021-10-21 15:48:23 +0900

    http 05

--- http.sh
+++ http.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
-sudo su -
-mkdir /web
-cd /web
-yum install -y wget
-yum install -y expat-devel gcc gcc-c++
-wget https://downloads.apache.org/apr/apr-1.7.0.tar.gz
-wget https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
-wget https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
-wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
-tar xvfz apr-1.7.0.tar.gz
-tar xvfz apr-util-1.6.1.tar.gz
-tar xvfz httpd-2.4.51.tar.gz
-tar xvfz pcre-8.45.tar.gz
-yum install -y expat-devel gcc gcc-c++
-cd apr-1.7.0
-./configure --prefix=/web/apr
-make
+sudo mkdir /web
+sudo cd /web
+sudo yum install -y wget
+sudo yum install -y expat-devel gcc gcc-c++
+sudo wget https://downloads.apache.org/apr/apr-1.7.0.tar.gz
+sudo wget https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
+sudo wget https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
+sudo wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
+sudo tar xvfz apr-1.7.0.tar.gz
+sudo tar xvfz apr-util-1.6.1.tar.gz
+sudo tar xvfz httpd-2.4.51.tar.gz
+sudo tar xvfz pcre-8.45.tar.gz
+sudo yum install -y expat-devel gcc gcc-c++
+sudo cd apr-1.7.0
+sudo ./configure --prefix=/web/apr
+sudo make
+sudo make install
+sudo cd ../apr-util-1.6.1
+sudo ./configure --prefix=/web/aprutil --with-apr=/web/apr
+sudo make
 make install
-cd ../apr-util-1.6.1
-./configure --prefix=/web/aprutil --with-apr=/web/apr
-make
-make install
-cd ../pcre-8.45
-./configure --prefix=/web/pcre
-make
-make install
-cd ../httpd-2.4.51
-./configure --prefix /usr/local/apache2 --with-apr=/web/apr --with-apr-util=/web/aprutil --with-pcre=/web/pcre
-make
-make install
-/usr/local/apache2/bin/apachectl start
+sudo cd ../pcre-8.45
+sudo ./configure --prefix=/web/pcre
+sudo make
+sudo make install
+sudo cd ../httpd-2.4.51
+sudo ./configure --prefix /usr/local/apache2 --with-apr=/web/apr --with-apr-util=/web/aprutil --with-pcre=/web/pcre
+sudo make
+sudo make install
+sudo /usr/local/apache2/bin/apachectl start
 
